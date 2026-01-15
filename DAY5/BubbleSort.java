d9b49f57f55f26505def496bbbd335f231df6ebf
nauditbisen <nauditbisen64@gmail.com>
2026-01-15 17:05:20 +0530

    Day5

---
+++ DAY5/BubbleSort.java
@@ -0,0 +1,25 @@
+package DAY5;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int arr[]={12,54,21,46,8,76,12};
+        int temp=0;
+
+        for(int i=0;i<arr.length;i++){
+        
+            for(int j=0;j<arr.length-1;j++){
+             if (arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+             }
+            }
+
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
